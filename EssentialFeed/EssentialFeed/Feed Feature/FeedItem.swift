8d37f56e9c4ecd0bf7222c7cb9990585da92a7dd
ontheeasiestway <ontheeasiestway@gmail.com>
2020-11-03 23:01:40 +0800

    Add struct FeedItem

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by liuzhijin on 2020/11/3.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let descritpion: String?
+    let location: String?
+    let imageURL: URL
+}
