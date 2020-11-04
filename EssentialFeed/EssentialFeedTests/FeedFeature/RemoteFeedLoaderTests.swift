a56457ad1fec44aa701dbfc39780c706ff829d25
ontheeasiestway <ontheeasiestway@gmail.com>
2020-11-05 02:54:58 +0800

    Add memory leak check for sut and client

--- EssentialFeed/EssentialFeedTests/FeedFeature/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedFeature/RemoteFeedLoaderTests.swift
@@ -93,10 +93,20 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     // MARK: -- Helpers
 
-    private func makeSUT(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "http://any-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
+
+        trackMemoryLeak(client, file: file, line: line)
+        trackMemoryLeak(sut, file: file, line: line)
+
         return (sut: sut, client: client)
+    }
+
+    private func trackMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should be nil, potential memory leak", file: file, line: line)
+        }
     }
 
     private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
