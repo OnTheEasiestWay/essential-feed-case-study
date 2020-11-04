a25234d543abdee4294412cd7f7325fd465da2c8
ontheeasiestway <ontheeasiestway@gmail.com>
2020-11-04 16:20:37 +0800

    Check RemoteFeedLoader invokes HTTPClient with correct URL

--- EssentialFeed/EssentialFeedTests/FeedFeature/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedFeature/RemoteFeedLoaderTests.swift
@@ -9,14 +9,16 @@ import XCTest
 import Foundation
 
 class RemoteFeedLoader {
+    private let url: URL
     private let client: HTTPClient
 
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
 
     func load() {
-        client.get(from: URL(string: "http://a-given-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -27,19 +29,21 @@ protocol HTTPClient {
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "http://any-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestsDataFromURL() {
+        let url = URL(string: "http://a-test-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
 
         sut.load()
 
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 
     // MARK: -- Helpers
