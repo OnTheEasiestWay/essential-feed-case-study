fed5c27b4b665d16673826eeaaf1e6b961991805
ontheeasiestway <ontheeasiestway@gmail.com>
2020-11-04 21:12:16 +0800

    Convert 200 status code and valid JSON data of HTTPClient to FeedItem array of RemoteFeedLoader

--- EssentialFeed/EssentialFeedTests/FeedFeature/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedFeature/RemoteFeedLoaderTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import Foundation
+import EssentialFeed
 
 class RemoteFeedLoader {
     private let url: URL
@@ -22,17 +23,32 @@ class RemoteFeedLoader {
         case invalidData
     }
 
-    func load(completion: @escaping (Error) -> Void) {
+    enum Result {
+        case success([FeedItem])
+        case failure(Error)
+    }
+
+    func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { result in
             switch (result) {
-            case .success:
-                completion(.invalidData)
+            case let .success(data, response):
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200,
+                      let root = try? JSONDecoder().decode(Root.self, from: data) else {
+                    completion(.failure(.invalidData))
+                    return
+                }
+
+                completion(.success(root.items))
 
             case .failure:
-                completion(.connectivity)
+                completion(.failure(.connectivity))
             }
         }
     }
+}
+
+struct Root: Decodable {
+    let items: [FeedItem]
 }
 
 enum HTTPClientResult {
@@ -92,6 +108,50 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
 
+    func test_load_succeedsOn200HTTPResponse() {
+        let url = URL(string: "http://a-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        let item1 = FeedItem(id: UUID(), description: nil, location: nil, imageURL: URL(string: "http://image-url1.com")!)
+        let item1JSON = [
+            "id": item1.id.uuidString,
+            "description": item1.description,
+            "location": item1.location,
+            "image": item1.imageURL.absoluteString
+        ].compactMapValues { $0 }
+        
+        let item2 = FeedItem(id: UUID(), description: "a description", location: "a location", imageURL: URL(string: "http:/image-url2.com")!)
+        let item2JSON = [
+            "id": item2.id.uuidString,
+            "description": item2.description,
+            "location": item2.location,
+            "image": item2.imageURL.absoluteString
+        ].compactMapValues { $0 }
+
+        let itemsJSON = [
+            "items": [
+                item1JSON,
+                item2JSON
+            ]
+        ]
+        let data = try! JSONSerialization.data(withJSONObject: itemsJSON)
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+
+        let exp = expectation(description: "Wait load to complete")
+        var capturedResult: [FeedItem]?
+        sut.load { result in
+            switch (result) {
+            case let .success(items):
+                capturedResult = items
+            case .failure:
+                XCTFail("Expected success, got \(result) instead", file: #filePath, line: #line)
+            }
+            exp.fulfill()
+        }
+        client.complete(with: (data: data, response: response))
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(capturedResult, [item1, item2])
+    }
+
     // MARK: -- Helpers
 
     private func makeSUT(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -100,11 +160,16 @@ class RemoteFeedLoaderTests: XCTestCase {
         return (sut: sut, client: client)
     }
 
-    private func expect(_ sut: RemoteFeedLoader, with error: RemoteFeedLoader.Error, when action: () -> Void) {
+    private func expect(_ sut: RemoteFeedLoader, with error: RemoteFeedLoader.Error, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         let exp = expectation(description: "Wait load to complete")
         var capturedError: RemoteFeedLoader.Error?
-        sut.load() { error in
-            capturedError = error
+        sut.load() { result in
+            switch (result) {
+            case let .failure(error):
+                capturedError = error
+            case .success:
+                XCTFail("Expected failure, got \(result) instead", file: file, line: line)
+            }
             exp.fulfill()
         }
 
@@ -137,6 +202,10 @@ class RemoteFeedLoaderTests: XCTestCase {
                     httpVersion: nil,
                     headerFields: nil)!
             messages[index].completion(.success(data, response));
+        }
+
+        func complete(with success: (data: Data, response: URLResponse), at index: Int = 0) {
+            messages[index].completion(.success(success.data, success.response))
         }
     }
 }
