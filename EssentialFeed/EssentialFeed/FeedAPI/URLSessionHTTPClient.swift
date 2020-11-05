8d1587e3c70f42262b61671a84940a7970633ad3
ontheeasiestway <ontheeasiestway@gmail.com>
2020-11-05 23:37:37 +0800

    Move URLSessionHTTPClient to production code

---
+++ EssentialFeed/EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
@@ -0,0 +1,30 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by liuzhijin on 2020/11/5.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+
+    public init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+
+    struct InvalidRepresentation: Error {}
+
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(InvalidRepresentation()))
+            }
+        }.resume()
+    }
+}
